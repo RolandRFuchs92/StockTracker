52d2cb10d781cdebaec2cb348ef7207f35316069
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-09-06 20:17:15 +0200

    Added edit and disable client interface

---
+++ StockTracker.BusinessLogic.Interface/BusinessLogic/Clients/IDisableClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockTracker.Interface.Models;
+
+namespace StockTracker.BusinessLogic.Interface.BusinessLogic.Clients
+{
+    public interface IDisableClient
+    {
+	    bool DisableClient(int clientId);
+	    bool DisableClient(IClient client);
+    }
+}
---
+++ StockTracker.BusinessLogic.Interface/BusinessLogic/Clients/IEditClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockTracker.Interface.Models;
+
+namespace StockTracker.BusinessLogic.Interface.BusinessLogic.Clients
+{
+    public interface IEditClient
+    {
+	    IClient RenameClient(int clientId, string name);
+	    IClient RenameClient(IClient client, string name);
+	    IClient Address(int clientId, string address);
+	    IClient Address(IClient client, string address);
+	    IClient Email(int clientId, string email);
+	    IClient Email(IClient clientId, string email);
+	    IClient Mobile(int clientId, string mobile);
+	    IClient Mobile(IClient client, string mobile);
+	    bool Disable(int clientId, bool isActive);
+
+    }
+}
