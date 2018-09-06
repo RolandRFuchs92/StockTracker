e4e3c6b0261e222fae3fa334c09f63944d2a2941
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-09-06 07:43:55 +0200

    Added several method additions via interface

---
+++ StockTracker.BusinessLogic.Interface/BusinessLogic/Clients/IGetClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockTracker.Interface.Models;
+using StockTracker.Interface.Models.User;
+
+namespace StockTracker.BusinessLogic.Interface.BusinessLogic.Clients
+{
+    public interface IGetClient
+    {
+	    IClient GetClient(int clientId);
+	    IClient GetClientByMember(int memberId);
+	    IClient GetClientByMember(IMember member);
+	    IClient GetClientByPerson(int personId);
+	    IClient GetClientByPerson(IPerson person);
+	    IClient GetClientByPerson(string name, string surname);
+	    IClient GetClient(string contactNumber);
+	    IClient GetClientByEmail(string email);
+    }
+}
