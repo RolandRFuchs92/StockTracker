a009088081bad241140dbe7c81dc99eec25b2872
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-09-06 20:44:34 +0200

    Added client settings interface

---
+++ StockTracker.BusinessLogic.Interface/BusinessLogic/Members/IUpdateMember.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockTracker.Interface.Models.User;
+
+namespace StockTracker.BusinessLogic.Interface.BusinessLogic.Members
+{
+    public interface IUpdateMember
+    {
+		
+    }
+}
---
+++ StockTracker.BusinessLogic.Interface/BusinessLogic/Settings/IGetSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockTracker.Interface.Models;
+using StockTracker.Interface.Models.Settings;
+
+namespace StockTracker.BusinessLogic.Interface.BusinessLogic.Settings
+{
+    public interface IGetSettings
+    {
+	    bool IsAnyoneAbleToAddStock(int clientId);
+	    bool IsAnyoneAbleToAddStock(IClient client);
+	    DateTime OpenTime(int clientId);
+	    DateTime CloseTime(int clientId);
+	    IClientSettings ClientSettings(int clientId);
+	    bool CanEmailManagers(int clientId);
+    }
+}
