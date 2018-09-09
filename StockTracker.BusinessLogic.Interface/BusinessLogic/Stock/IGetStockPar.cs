5d0ca00219dbc28379e3e04e1ead28fa148b015b
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-09-09 13:22:17 +0200

    Added interface to get stockpars.

--- StockTracker.BusinessLogic.Interface/BusinessLogic/Stock/IGetStockPar.cs
+++ StockTracker.BusinessLogic.Interface/BusinessLogic/Stock/IGetStockPar.cs
@@ -10,5 +10,8 @@ namespace StockTracker.BusinessLogic.Interface.BusinessLogic.Stock
     public interface IGetStockPar
     {
 	    IStockPar Get(int stockItemId, int clientId);
+	    IStockPar Get(IStockLevel stockLevel);
+	    List<IStockPar> Get(List<int> stockItems, int clientId);
+		List<IStockPar> Get(List<IStockLevel> stockLevels);
     }
 }
