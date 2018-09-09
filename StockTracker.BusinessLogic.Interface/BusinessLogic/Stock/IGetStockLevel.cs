ad6f16e7986a6c0f26b751098863469ae034ea79
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-09-09 12:54:52 +0200

    Added Businesslogic to help get Stock Levels

---
+++ StockTracker.BusinessLogic.Interface/BusinessLogic/Stock/IGetStockLevel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockTracker.Interface.Models.Stock;
+
+namespace StockTracker.BusinessLogic.Interface.BusinessLogic.Stock
+{
+    public interface IGetStockLevel
+    {
+	    IStockLevel Get(int stockItemId, int clientId);
+	    IStockLevel Get(IStockItem stockItem);
+	    List<IStockLevel> Get(List<int> stockItemId, int clientId);
+	    List<IStockLevel> Get(List<IStockItem> stockItems);
+    }
+}
