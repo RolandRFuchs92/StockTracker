8bf0b31d31d6df8584ae5ba1e3d0040ad5a2f55f
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-09-10 20:14:36 +0200

    Wrote all the tests finished for updateshoppinglisttest

--- StockTracker.Tests/StockTracker/Shopping/UpdateShoppingListTests.cs
+++ StockTracker.Tests/StockTracker/Shopping/UpdateShoppingListTests.cs
@@ -54,5 +54,83 @@ namespace StockTracker.Test.StockTracker.Shopping
 			Assert.IsNotNull(result);
 			Assert.IsInstanceOfType(result, typeof(IShoppingList));
 		}
+
+		[TestMethod]
+		public void Remove_RemovedItemFromShoppingList_ShouldHaveDeletedAnItemFromShoppingList()
+		{
+			//Arrange
+			var shoppingListId = 1;
+			var shoppingListRef = _db.ShoppingLists.FirstOrDefault(i => i.ShoppingListId == shoppingListId);
+			var originalList = _db.ShoppingListItems.Where(i => i.ShoppingListId == shoppingListRef.ShoppingListId).ToList();
+			var stockItemId = originalList.FirstOrDefault().StockItemId;
+
+			//Act
+			var result = _updateShoppingList.Remove(shoppingListRef.ShoppingListId, stockItemId);
+			var updatedList = _db.ShoppingListItems.Where(i => i.ShoppingListId == shoppingListRef.ShoppingListId);
+
+			//Assert
+			Assert.IsNotNull(result);
+			Assert.IsInstanceOfType(result, typeof(IShoppingList));
+			Assert.IsTrue(originalList.Count > updatedList.Count());
+		}
+
+		[TestMethod]
+		public void Remove_PassedShoppingListRefAndAddedAListOfItemsToBeremoved_ShouldReturnAListExcludingReffedItems()
+		{
+			//Arrange
+			var shoppingListId = 1;
+			var shoppingListRef = _db.ShoppingLists.FirstOrDefault(i => i.ShoppingListId == shoppingListId);
+			var originalList = _db.ShoppingListItems.Where(i => i.ShoppingListId == shoppingListId).ToList();
+			var stockItemIds = originalList.Take(2).Select(i => i.StockItemId).ToList();
+
+			//Act
+			var result = _updateShoppingList.Remove(shoppingListRef.ShoppingListId, stockItemIds);
+			var updatedList = _db.ShoppingListItems.Where(i => i.ShoppingListId == shoppingListRef.ShoppingListId).ToList();
+
+			//Assert
+			Assert.IsNotNull(result);
+			Assert.IsInstanceOfType(result, typeof(IShoppingList));
+			Assert.IsTrue(originalList.Count > updatedList.Count);
+		}
+
+		[TestMethod]
+		public void Update_PassedShoppingListItemIdWithStockItemRefAndNewStockitem_ReturnShoppingListRef()
+		{
+			//Arrange
+			var shoppingListId = 1;
+			var shoppingListRef = _db.ShoppingLists.FirstOrDefault(i => i.ShoppingListId == shoppingListId);
+			var originalList = _db.ShoppingListItems.Where(i => i.ShoppingListId == shoppingListRef.ShoppingListId).ToList();
+			var originalShoppingItemIds = originalList.Select(i => i.ShoppingListId).ToList();
+			var originShoppingItemId = originalList.FirstOrDefault(i => i.ShoppingListId == shoppingListId).ShoppingListId;
+			var newShoppingItemId = GetNewShoppingListId(originalShoppingItemIds);
+
+			//Act
+			var result = _updateShoppingList.Update(shoppingListId, originShoppingItemId, newShoppingItemId);
+			var newShoppingList = _db.ShoppingListItems.Where(i => i.ShoppingListId == shoppingListRef.ShoppingListId).ToList();
+
+			//Assert
+			Assert.IsNotNull(result);
+			Assert.IsInstanceOfType(result, typeof(IShoppingList));
+			Assert.IsTrue(CompareLists(originalList, newShoppingList));
+		}
+
+		private int GetNewShoppingListId(List<int> originalShoppingListId)
+		{
+			var newShoppingItemId = 1;
+			while(originalShoppingListId.Contains(newShoppingItemId))
+			{
+				newShoppingItemId++;
+			}
+
+			return newShoppingItemId;
+		}
+
+		private bool CompareLists(List<ShoppingListItem> originalList, List<ShoppingListItem> updateList)
+		{
+			var difference = originalList.Except(updateList).Count();
+			if (difference == 0)
+				return true;
+			return false;
+		}
 	}
 }
