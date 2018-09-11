b70c5e172849260eaef744fa9fd7511d3db2f420
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-09-11 20:41:52 +0200

    Fixed a Unit Test, adjusted older ones and added 2 new unit tests for the up and coming disablestock repo.

--- StockTracker.Tests/StockTracker/Stock/DisableStockTest.cs
+++ StockTracker.Tests/StockTracker/Stock/DisableStockTest.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StockTracker.Context;
+using StockTracker.Interface.Models.Stock;
 using StockTracker.Model.Stock;
 using StockTracker.Repository.Interface.BusinessLogic.Stock;
 
@@ -26,16 +27,17 @@ namespace StockTracker.Repository.Test.StockTracker.Stock
 			//Arrange
 			var stockItemId = 1;
 			var clientId = 1;
-			var originalActiveItem = _db.StockLevels.FirstOrDefault(i => i.StockItemId == stockItemId && i.ClientId == clientId);
+			var originalActiveItem = _db.StockPars.FirstOrDefault(i => i.StockItemId == stockItemId && i.ClientId == clientId);
 
 			//Act
 			var result = _disable.Disable(stockItemId, clientId);
-			var updateItem = _db.StockLevels.FirstOrDefault(i => i.StockItemId == stockItemId && i.ClientId == clientId);
+			var updateItem = _db.StockPars.FirstOrDefault(i => i.StockItemId == stockItemId && i.ClientId == clientId);
 
 			//Assert
 			Assert.IsNotNull(result);
 			Assert.IsInstanceOfType(result, typeof(StockLevel));
 			Assert.IsTrue(result);
+			Assert.IsTrue(originalActiveItem.IsActive != updateItem.IsActive);
 		}
 
 		[TestMethod]
@@ -43,15 +45,18 @@ namespace StockTracker.Repository.Test.StockTracker.Stock
 		{
 			//Arrange
 			var clientId = 1;
-			var listOfStockIds = _db.StockLevels.Where(i => i.ClientId == clientId).ToList();
+			var listOfStockLevels= _db.StockPars.Where(i => i.ClientId == clientId).ToList();
+			var listOfStockIds = listOfStockLevels.Select(i => i.StockItemId).ToList();
 
 
 			//Act
-			var result = _disable.Disable(listOfStockIds.Select(i => i.StockItemId).ToList(), clientId);
+			var result = _disable.Disable(listOfStockIds, clientId);
+			var updateListOfStocks = _db.StockPars.Where(i => i.ClientId == clientId).ToList();
 
 			//Assert
 			Assert.IsNotNull(result);
 			Assert.IsTrue(result);
+			Assert.IsNull(updateListOfStocks.FirstOrDefault(i => i.IsActive));
 		}
 
 
@@ -62,12 +67,34 @@ namespace StockTracker.Repository.Test.StockTracker.Stock
 		    var clientId = 1;
 		    var categoryId = 1;
 
+
 		    //Act
 		    var result = _disable.DisableByCategory(categoryId, clientId);
+		    var updatedList = _db.StockPars.Where(i => i.ClientId == clientId && i.StockItem.StockCategoryId == categoryId).ToList();
 
-		    //Assert
+			//Assert
 			Assert.IsNotNull(result);
 			Assert.IsTrue(result);
+			Assert.IsNull(updatedList.FirstOrDefault(i => i.IsActive).IsActive);
 	    }
+
+		[TestMethod]
+		public void DisableByCategory_PassedInAListOfCategories_AllListsShouldBeDisabled()
+		{
+			//Arrange
+			var clientId = 1;
+			var categoryIds = new List<int> {1, 2, 3};
+			//should add a constant list to the Db generator so that we can test specifics.
+
+			//Act
+			var result = _disable.DisableByCategory(categoryIds, clientId);
+			var updatedList =
+				_db.StockPars.Where(i => categoryIds.Contains(i.StockItem.StockCategoryId) && i.ClientId == clientId).Select(i => i.IsActive);
+
+			//Assert
+			Assert.IsNotNull(result);
+			Assert.IsTrue(result);
+			Assert.IsNull(updatedList.FirstOrDefault());
+		}
     }
 }
