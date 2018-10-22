a6459467b3526b375942d84ae23b0dfd7435eca1
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-10-22 19:05:51 +0200

    Added  SupplierType model

---
+++ StockTracker.Interface/Models/Supplier/SupplierType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTracker.Interface.Models.Supplier
+{
+    public class SupplierType
+    {
+	    public int SupplierTypeId { get; set; }
+	    public string SupplierTypeName { get; set; }
+    }
+}
