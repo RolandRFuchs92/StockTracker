497c4b06f2b16b41523bc5e2edb1aa73a4026771
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-10-22 19:11:50 +0200

    Added IUnitType Interface

---
+++ StockTracker.Interface/Models/Unit/IUnitType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTracker.Interface.Models.Unit
+{
+	public interface IUnitType
+	{
+		int UnitType { get; set; }
+		string Name { get; set; }
+		string Symbol { get; set; }
+	}
+}
