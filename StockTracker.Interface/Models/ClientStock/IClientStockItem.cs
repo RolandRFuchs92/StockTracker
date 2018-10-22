839c8767d83bfbbde7115b061627c11641f22385
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-10-22 20:39:46 +0200

    Added ClientStockIntefaces

---
+++ StockTracker.Interface/Models/ClientStock/IClientStockItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTracker.Interface.Models.ClientStock
+{
+    public interface IClientStockItem
+    {
+        int ClientStockItemId { get; set; }
+		int StockCoreId { get; set; }
+		int ClientId { get; set; }
+		int StockMax { get; set; }
+		int StockMin { get; set; }
+		int CreatedOn { get; set; }
+		bool IsActive { get; set; }
+    }
+}
---
+++ StockTracker.Interface/Models/ClientStock/IClientStockLevel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTracker.Interface.Models.ClientStock
+{
+    public interface IClientStockLevel
+    {
+        int ClientStockLevelId { get; set; }
+		int ClientStockItemId { get; set; }
+		int MemberId { get; set; }
+		int Quantity { get; set; }
+		DateTime CreatedOn { get; set; }
+		bool IsActive { get; set; }
+    }
+}
