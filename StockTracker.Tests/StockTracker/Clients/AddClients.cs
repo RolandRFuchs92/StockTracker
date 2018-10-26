05a9245267f94cc759d2c6faeeeb52ba2cb8f385
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-10-26 06:32:41 +0200

    rebuilding our app\

---
+++ StockTracker.Tests/StockTracker/Clients/AddClients.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using StockTracker.Context;
+using StockTracker.Context.Interface;
+using StockTracker.Interface.Models.Client;
+using StockTracker.Model.Clients;
+using StockTracker.Repository.Interface.BusinessLogic.Clients;
+
+namespace StockTracker.Repository.Test.StockTracker.Clients
+{
+    public class AddClients : IAddClient
+    {
+	    private IStockTrackerContext _db;
+
+	    public AddClients(IStockTrackerContext db)
+	    {
+		    _db = db;
+	    }
+
+	    public bool AddClient(IClient newClient)
+	    {
+		    try
+		    {
+			    _db.Clients.Add((Client) newClient);
+			    ((StockTrackerContext) _db).SaveChanges();
+				return true;
+		    }
+		    catch (Exception E)
+		    {
+			    return false;
+		    }
+
+	    }
+
+		public bool AddClient(bool isActive, string name, string email, int contactNumber)
+	    {
+		    throw new NotImplementedException();
+	    }
+    }
+}
