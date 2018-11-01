4e2f6ef4e80f47a8105b74c018ea376895a479c6
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-11-01 22:17:52 +0200

    Added tests for validating numbers and emails.

--- StockTracker.Extensions.Test/StringExtensions/ValidateTest.cs
+++ StockTracker.Extensions.Test/StringExtensions/ValidateTest.cs
@@ -16,7 +16,7 @@ namespace StockTracker.Extensions.Test.StringExtensions
 		{
 			//Arrange
 			var result = false;
-			var emailList = new []
+			var emailList = new string[]
 			{
 				"roland@ix.co.za",
 				"rolandrfuchs92@gmail.com",
@@ -38,5 +38,114 @@ namespace StockTracker.Extensions.Test.StringExtensions
 			Assert.IsTrue(result);
 		}
 
-    }
+		[TestMethod]
+		public void IsValidEmail_InvalidListOfEmails_False()
+		{
+			//Arrange
+			var result = false;
+			var lastEmail = "";
+			var emailList = new []
+			{
+				"plainaddress",
+				"#@%^%#$@#$@#.com",
+				"@domain.com",
+				"Joe Smith <email@domain.com>",
+				"email.domain.com",
+				".email@domain.com",
+				"email.@domain.com",
+				"email..email@domain.com",
+				"あいうえお@domain.com",
+				"email@domain.com (Joe Smith)",
+				"email@domain",
+				"email@-domain.com",
+				"email@domain.web",
+				"r@at@.@moo",
+				"roland@moo.zoo$coo.doo",
+				"email@111.222.333.44444",
+				"email@domain..com",
+				"rolandatix.co.za"
+			};
+
+			//Act
+			foreach (var email in emailList)
+			{
+				result = email.IsValidEmail();
+				if (result)
+				{
+					lastEmail = email;
+					break;
+				}
+			}
+
+			//Assert
+			Assert.IsFalse(result, $"{lastEmail} was the last email validated.");
+		}
+
+	    [TestMethod]
+	    public void IsPhoneNumberValid_PassValidMobileNumbers_True()
+	    {
+		    //Arrange
+		    var result = false;
+		    var lastMobile = "";
+		    var validMobileNumbers = new[]
+		    {
+				"+27123456987",
+				"0831234569",
+				"083 123 4569",
+				"(073) 123 4569",
+				"083-123-4567",
+				"08 31 23 45 67"
+		    };
+
+
+		    //Act
+		    foreach (var validMobileNumber in validMobileNumbers)
+		    {
+				result = validMobileNumber.IsPhoneNumberValid();
+			    if (!result)
+			    {
+				    lastMobile = validMobileNumber;
+				    break;
+			    }
+		    }
+
+			//Assert
+			Assert.IsTrue(result, $"{lastMobile} was the last number checked.");
+		}
+
+		[TestMethod]
+		public void IsPhoneNumberInvalid_PassInvalidMobileNumbers_False()
+		{
+			//Arrange
+			var result = false;
+			var lastMobile = "";
+			var invalidPoneNumberList = new[]
+			{
+				"as1234567890",
+				"#1234567890",
+				"123_123_1234",
+				"##1231",
+				"123",
+				"12345678",
+				"123+321+1234",
+				"this@somemail.com",
+				
+			};
+
+			//Act
+			foreach (var number in invalidPoneNumberList)
+			{
+				result = number.IsPhoneNumberValid();
+				if (result)
+				{
+					lastMobile = number;
+					break;
+				}
+			}
+
+			//Assert
+			Assert.IsFalse(result, $"{lastMobile} was the last number checked.");
+
+		}
+	}
 }
--- StockTracker.Extensions/StringExtensions/Validate.cs
+++ StockTracker.Extensions/StringExtensions/Validate.cs
@@ -14,7 +14,35 @@ namespace StockTracker.Extensions.StringExtensions
 			var rx = new Regex(
 				@"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$");
 
-			return rx.IsMatch(email);
+			if (email.EndsWith(".web"))
+				return false;
+
+			if (!rx.IsMatch(email))
+				return false;
+
+			var domain = email.Split("@")[1].Split(".");
+
+			foreach (var splits in domain)
+			{
+				if (Regex.IsMatch(splits, @"\d") && domain.Length == 4 &&int.Parse(splits) > 255)
+					return false;
+			}
+
+			return true;
+		}
+
+		public static bool IsPhoneNumberValid(this string phoneNumber)
+		{
+			 if (Regex.IsMatch(phoneNumber, @"[0-9]"))
+				return false;
+
+			var rx = new Regex(@"^\+?[1-9][0-9\s.-]{7,11}$");
+			if (rx.IsMatch(phoneNumber))
+				return true;
+
+			rx = new Regex(@"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}");
+
+			return rx.IsMatch(phoneNumber.Replace(" ", ""));
 		}
 	}
 }
