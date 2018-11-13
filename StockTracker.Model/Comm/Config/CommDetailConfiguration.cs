a242a4f31c7708da606d28b65e80caaaff1bc132
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-11-13 20:46:55 +0200

    Added CommDetailConfiguration

---
+++ StockTracker.Model/Comm/Config/CommDetailConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace StockTracker.Model.Comm.Config
+{
+    public class CommDetailConfiguration : IEntityTypeConfiguration<CommDetail>
+    {
+	    public void Configure(EntityTypeBuilder<CommDetail> builder)
+	    {
+		    builder.HasKey(i => i.CommDetailId);
+
+		    builder.HasOne(i => i.Member);
+		    builder.HasOne(i => i.CommError);
+
+		    builder.Property(i => i.CommDetailId).HasColumnType("Int").IsRequired().ValueGeneratedOnAdd();
+		    builder.Property(i => i.CommErrorId).HasColumnType("Int").IsRequired();
+		    builder.Property(i => i.Message).HasColumnType("NVARCHAR(MAX)").IsRequired();
+		    builder.Property(i => i.Recipients).HasColumnType("NVARCHAR(2048)").IsRequired();
+		    builder.Property(i => i.Sender).HasColumnType("NVARCHAR(200)").IsRequired();
+		    builder.Property(i => i.Response).HasColumnType("NVARCHAR(2048)").IsRequired(false);
+		    builder.Property(i => i.Subject).HasColumnType("NVARCHAR(256)").IsRequired();
+		    builder.Property(i => i.CommError).HasColumnType("NVARCHAR(MAX)").IsRequired(false);
+		    builder.Property(i => i.MemberId).HasColumnType("INT").IsRequired();
+	    }
+    }
+}
+
