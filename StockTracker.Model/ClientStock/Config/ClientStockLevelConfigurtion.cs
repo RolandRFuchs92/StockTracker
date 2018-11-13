b2c2ac5836c16138182779c760a0f8a107c286d5
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-11-13 19:44:18 +0200

    Added ClientStockLevelConfigureation.

--- StockTracker.Model/ClientStock/Config/ClientStockLevelConfigurtion.cs
+++ StockTracker.Model/ClientStock/Config/ClientStockLevelConfigurtion.cs
@@ -8,11 +8,20 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace StockTracker.Model.ClientStock.Config
 {
-    public class ClientStockLevelConfigurtion : IEntityTypeConfiguration<ClientStockLevel>
-    {
-	    public void Configure(EntityTypeBuilder<ClientStockLevel> builder)
-	    {
-		    throw new NotImplementedException();
-	    }
-    }
+	public class ClientStockLevelConfigurtion : IEntityTypeConfiguration<ClientStockLevel>
+	{
+		public void Configure(EntityTypeBuilder<ClientStockLevel> builder)
+		{
+			builder.HasKey(i => i.ClientStockLevelId);
+			builder.HasOne(i => i.Member);
+			builder.HasOne(i => i.ClientStockItem);
+
+
+			builder.Property(i => i.ClientStockItemId).HasColumnType("Int").IsRequired();
+			builder.Property(i => i.CreatedOn).HasColumnType("DateTime").IsRequired();
+			builder.Property(i => i.IsActive).HasColumnType("Bit").IsRequired();
+			builder.Property(i => i.MemberId).HasColumnType("Int").IsRequired();
+			builder.Property(i => i.Quantity).HasColumnType("Int").IsRequired();
+		}
+	}
 }
