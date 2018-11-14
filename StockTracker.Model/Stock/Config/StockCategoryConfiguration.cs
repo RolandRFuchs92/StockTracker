061084ec8a6b93bdc2ac1fce8e60aac8ddf6a185
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-11-14 20:27:55 +0200

    Added StockCategoryConfiguration, Added StockCoreConfiguration and added StockTypeConfiguration

---
+++ StockTracker.Model/Stock/Config/StockCategoryConfiguration.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace StockTracker.Model.Stock.Config
+{
+	public class StockCategoryConfiguration : IEntityTypeConfiguration<StockCategory>
+	{
+		public void Configure(EntityTypeBuilder<StockCategory> builder)
+		{
+			builder.HasKey(i => i.StockCategoryId);
+
+			builder.Property(i => i.StockCategoryId).IsRequired().HasColumnType("INT").ValueGeneratedOnAdd();
+			builder.Property(i => i.StockCategoryName).IsRequired().HasColumnType("NVARCHAR(250)");
+
+			builder.HasData(GetStockCategorySeed());
+		}
+
+		StockCategory[] GetStockCategorySeed()
+		{
+			var viewModel = new[]
+			{
+				new StockCategory
+				{
+					StockCategoryId = 1,
+					StockCategoryName = "Meat"
+				},
+				new StockCategory
+				{
+					StockCategoryId = 2,
+					StockCategoryName = "Pasta"
+				},
+				new StockCategory
+				{
+					StockCategoryId = 3,
+					StockCategoryName = "Vegetable"
+				},
+				new StockCategory
+				{
+					StockCategoryId = 4,
+					StockCategoryName = "Powder"
+				},
+				new StockCategory
+				{
+					StockCategoryId = 5,
+					StockCategoryName = "Canned Good"
+				},
+				new StockCategory
+				{
+					StockCategoryId = 6,
+					StockCategoryName = "Fruit"
+				},
+				new StockCategory
+				{
+					StockCategoryId = 7,
+					StockCategoryName = "Oil"
+				},
+				new StockCategory
+				{
+					StockCategoryId = 7,
+					StockCategoryName = "Edible Liquid"
+				},
+			};
+
+			return viewModel;
+		}
+	}
+}
---
+++ StockTracker.Model/Stock/Config/StockTypeConfiguration.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace StockTracker.Model.Stock.Config
+{
+    public class StockTypeConfiguration : IEntityTypeConfiguration<StockType>
+    {
+	    public void Configure(EntityTypeBuilder<StockType> builder)
+	    {
+		    builder.HasKey(i => i.StockTypeId);
+
+		    builder.Property(i => i.StockTypeId).HasColumnType("INT").IsRequired().ValueGeneratedOnAdd();
+		    builder.Property(i => i.StockTypeName).HasColumnType("NVARCHAR(200)").IsRequired();
+
+		    builder.HasData(GetStockTypeSeedData());
+	    }
+
+	    StockType[] GetStockTypeSeedData()
+	    {
+		    var viewModel = new[]
+		    {
+			    new StockType
+			    {
+					StockTypeId = 1,
+					StockTypeName = "Frozen Treat"
+			    },
+			    new StockType
+			    {
+				    StockTypeId = 2,
+				    StockTypeName = "Raw Fruit"
+			    },
+			    new StockType
+			    {
+				    StockTypeId = 3,
+				    StockTypeName = "Sugar"
+			    },
+			    new StockType
+			    {
+				    StockTypeId = 4,
+				    StockTypeName = "Cooking Oil"
+			    },
+			    new StockType
+			    {
+				    StockTypeId = 5,
+				    StockTypeName = "Chicken"
+			    },
+			    new StockType
+			    {
+				    StockTypeId = 6,
+				    StockTypeName = "Fish"
+			    },
+			    new StockType
+			    {
+				    StockTypeId = 7,
+				    StockTypeName = "Pork"
+			    },
+			    new StockType
+			    {
+				    StockTypeId = 8,
+				    StockTypeName = "Beef"
+			    },
+			    new StockType
+			    {
+				    StockTypeId = 9,
+				    StockTypeName = "Sauce"
+			    },
+			    new StockType
+			    {
+				    StockTypeId = 10,
+				    StockTypeName = "Spice"
+			    },
+			    new StockType
+			    {
+				    StockTypeId = 11,
+				    StockTypeName = "Canned Fruit"
+			    },
+			    new StockType
+			    {
+				    StockTypeId = 12,
+				    StockTypeName = "Canned Vegetable"
+			    },
+			    new StockType
+			    {
+				    StockTypeId = 13,
+				    StockTypeName = "Canned Meat"
+			    },
+			    new StockType
+			    {
+				    StockTypeId = 14,
+				    StockTypeName = "Soda"
+			    },
+			    new StockType
+			    {
+				    StockTypeId = 15,
+				    StockTypeName = "Fruit Juice"
+			    },
+			    new StockType
+			    {
+				    StockTypeId = 16,
+				    StockTypeName = "Beer"
+			    },
+			    new StockType
+			    {
+				    StockTypeId = 17,
+				    StockTypeName = "Wine"
+			    },
+			    new StockType
+			    {
+				    StockTypeId = 18,
+				    StockTypeName = "Cider"
+			    },
+			    new StockType
+			    {
+				    StockTypeId = 19,
+				    StockTypeName = "Water"
+			    },
+			    new StockType
+			    {
+				    StockTypeId = 20,
+				    StockTypeName = "Yogurt"
+			    },
+			    new StockType
+			    {
+				    StockTypeId = 21,
+				    StockTypeName = "Cream"
+			    },
+			};
+
+
+		    return viewModel;
+	    }
+    }
+}
