8e14b0290e86d364ca665f6c2a918c2d566cf585
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-11-19 21:12:48 +0200

    Moved the ClientSettingsRepoTest around

--- StockTracker.Tests/StockTracker/Clients/ClientSettingsRepoTest.cs
+++ StockTracker.Tests/StockTracker/Clients/ClientSettingsRepoTest.cs
@@ -3,208 +3,235 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using StockTracker.Context;
 using StockTracker.Context.Interface;
 using StockTracker.Interface.Models.Clients;
+using StockTracker.Model.Clients;
+using StockTracker.Model.ClientStock;
+using StockTracker.Model.Member;
+using StockTracker.Model.Person;
+using StockTracker.Model.Shopping;
+using StockTracker.Model.Stock;
 using StockTracker.Repository.Clients;
 using StockTracker.Repository.Interface.Clients;
 using StockTracker.Seed.Clients;
 
 namespace StockTracker.Repository.Test.StockTracker.Clients
 {
-		public class ClientSettingsRepoTest
+	[TestClass]
+	public class ClientSettingsRepoTest
+	{
+		private IStockTrackerContext _db;
+		private GenericClientSettings _genericSettings;
+		private IClientSettingsRepo _clientSettingsRepo;
+		private GenericClients _clients;
+
+		public ClientSettingsRepoTest()
 		{
-				private Mock<IStockTrackerContext> _db;
-				private GenericClientSettings _genericSettings;
-				private IClientSettingsRepo _clientSettingsRepo;
-
-				public ClientSettingsRepoTest()
-				{
-						_db = new Mock<IStockTrackerContext>();
-						_genericSettings = new GenericClientSettings();
-				}
-
-				#region AddClientSettings Test
-				[TestMethod]
-				public void AddClientSettings_PassValidClientSettings_ReturnTrue()
-				{
-						//Arrange
-						var clientSettings = _genericSettings.One();
-
-						//Act
-						var result = _clientSettingsRepo.AddClientSettings(clientSettings);
-
-						//Assert
-						Assert.IsInstanceOfType(result, typeof(IClientSettings));
-						Assert.IsNull(result);
-						Assert.AreSame(result, clientSettings);
-				}
-
-				[TestMethod]
-				public void AddClientSettings_PassInvalidClientId_ReturnsFalse()
-				{
-						//Arrange
-						var clientSettings = _genericSettings.One();
-						clientSettings.ClientId = 0;
-
-						//Act
-						var result = _clientSettingsRepo.AddClientSettings(clientSettings);
-
-						//Assert
-						Assert.IsInstanceOfType(result, typeof(IClientSettings));
-						Assert.IsNull(result);
-						Assert.AreSame(result, clientSettings);
-				}
-				#endregion
-
-				#region IsActive Test
-				[TestMethod]
-				public void IsActive_PassClientIdAndValidActive_True()
-				{
-						//Arrange
-						var isActive = false;
-
-						//Act
-						var result = _clientSettingsRepo.IsActive(1, isActive);
-
-						//Assert
-						Assert.IsInstanceOfType(result, typeof(IClientSettings));
-						Assert.IsNotNull(result);
-						Assert.AreEqual(result.IsActive, isActive);
-				}
-
-
-				[TestMethod]
-				public void IsActive_PassInvalidClientId_false()
-				{
-						//Arrange
-						var isActive = false;
-
-						//Act
-						var result = _clientSettingsRepo.IsActive(0, isActive);
-
-						//Assert
-						Assert.IsInstanceOfType(result, typeof(IClientSettings));
-						Assert.IsNull(result);
-						Assert.AreNotEqual(result.IsActive, isActive);
-				}
-				#endregion
-
-				#region IsDeleted Test
-				[TestMethod]
-				public void IsDeleted_ValidClientIdAndFalse_True()
-				{
-						//Arrange
-						var isDeleted = false;
-
-						//Act
-						var result = _clientSettingsRepo.IsDeleted(1, isDeleted);
-
-						//Assert
-						Assert.IsInstanceOfType(result, typeof(IClientSettings));
-						Assert.IsNotNull(result);
-						Assert.AreEqual(result.IsDeleted, isDeleted);
-				}
-
-				[TestMethod]
-				public void IsDeleted_InvalidClientIdAndFalse_True()
-				{
-						//Arrange
-						var isDeleted = false;
-
-						//Act
-						var result = _clientSettingsRepo.IsDeleted(0, false);
-
-						//Assert
-						Assert.IsInstanceOfType(result, typeof(IClientSettings));
-						Assert.IsNull(result);
-				}
-				#endregion
-
-				#region Edit Test
-				[TestMethod]
-				public void Edit_NewClientSettingsValidClientId_true()
-				{
-						//Arrange
-						var clientSettings = _genericSettings.One();
-
-						//Act
-						var result = _clientSettingsRepo.Edit(clientSettings, 1);
-
-						//Assert
-						Assert.IsInstanceOfType(result, typeof(IClientSettings));
-						Assert.IsNull(result);
-						Assert.AreSame(result, clientSettings);
-				}
-
-				[TestMethod]
-				public void Edit_NewClientSettingsInvalidClientId_False()
-				{
-						//Arrange
-						var clientSettings = _genericSettings.One();
-
-						//Act
-						var result = _clientSettingsRepo.Edit(clientSettings, 0);
-
-						//Assert
-						Assert.IsInstanceOfType(result, typeof(IClientSettings));
-						Assert.IsNull(result);
-						Assert.AreSame(result, clientSettings);
-				}
-
-				#endregion
-
-				#region SetOpenClosedTimes Test
-				[TestMethod]
-				public void SetOpenClosedTimes_PassValidClientIdAndDateTime_True()
-				{
-						//Arrange
-						var openingTime = new DateTime(2018, 1, 1, 8, 0, 0);
-						var closingTime = new DateTime(2018, 1, 1, 17, 0, 0);
-
-						//Act
-						var result = _clientSettingsRepo.SetOpenClosedTimes(openingTime, closingTime, 1);
-
-						//Assert
-						Assert.IsInstanceOfType(result, typeof(IClientSettings));
-						Assert.IsNotNull(result);
-						Assert.AreSame(result.OpenTime, openingTime);
-						Assert.AreSame(result.CloseTime, closingTime);
-				}
-
-				[TestMethod]
-				public void SetOpenClosedTimes_PassInvalidClientIdAndDateTime_False()
-				{
-						//Arrange
-						var openingTime = new DateTime(2018, 1, 1, 8, 0, 0);
-						var closingTime = new DateTime(2018, 1, 1, 17, 0, 0);
-
-						//Act
-						var result = _clientSettingsRepo.SetOpenClosedTimes(openingTime, closingTime, 0);
-
-						//Assert
-						Assert.IsInstanceOfType(result, typeof(IClientSettings));
-						Assert.IsNull(result);
-				}
-				#endregion
-
-				#region AddTotalUser Test
-
-				[TestMethod]
-				public void AddTotalUsers_AddNumberOfNewUsers_True()
-				{
-						//Arrange
-						var clientSettings = _genericSettings.One();
-						
-						//Act
-
-
-						//Assert
-
-				}
-
-				#endregion
+			_db = new TestDb().Db;
+			_genericSettings = new GenericClientSettings();
+			_clientSettingsRepo = new ClientSettingsRepo(_db);
+			_clients = new GenericClients();
 		}
+
+		#region AddClientSettings Test
+		[TestMethod]
+		public void AddClientSettings_PassValidClientSettings_ReturnTrue()
+		{
+			//Arrange
+			AddClient();
+
+			var clientSettings = _genericSettings.One();
+			var clientSettingsRepo = new ClientSettingsRepo(_db);
+
+			//Act
+			var result = (ClientSettings)clientSettingsRepo.AddClientSettings(clientSettings);
+
+			//Assert
+			Assert.IsInstanceOfType(result, typeof(IClientSettings));
+			Assert.IsNotNull(result);
+			Assert.AreSame(result, clientSettings);
+		}
+
+		[TestMethod]
+		public void AddClientSettings_PassInvalidClientId_ReturnsFalse()
+		{
+			//Arrange
+			((StockTrackerContext)_db).Database.EnsureDeleted();
+			var clientSettings = _genericSettings.One();
+
+			//Act
+			var result = _clientSettingsRepo.AddClientSettings(clientSettings);
+
+			//Assert
+			Assert.IsNull(result);
+		}
+		#endregion
+
+		#region IsActive Test
+		[TestMethod]
+		public void IsActive_PassClientIdAndValidActive_True()
+		{
+			//Arrange
+			var client = AddClient();
+			var isActive = false;
+
+			//Act
+			var result = _clientSettingsRepo.IsActive(client, isActive);
+
+			//Assert
+			Assert.IsInstanceOfType(result, typeof(IClientSettings));
+			Assert.IsNotNull(result);
+			Assert.AreEqual(result.IsActive, isActive);
+		}
+
+
+		[TestMethod]
+		public void IsActive_PassInvalidClientId_false()
+		{
+			//Arrange
+			var isActive = false;
+
+			//Act
+			var result = _clientSettingsRepo.IsActive(0, isActive);
+
+			//Assert
+			Assert.IsNull(result);
+		}
+		#endregion
+
+		#region IsDeleted Test
+		[TestMethod]
+		public void IsDeleted_ValidClientIdAndFalse_True()
+		{
+			//Arrange
+			var isDeleted = false;
+
+			//Act
+			var result = _clientSettingsRepo.IsDeleted(1, isDeleted);
+
+			//Assert
+			Assert.IsInstanceOfType(result, typeof(IClientSettings));
+			Assert.IsNotNull(result);
+			Assert.AreEqual(result.IsDeleted, isDeleted);
+		}
+
+		[TestMethod]
+		public void IsDeleted_InvalidClientIdAndFalse_True()
+		{
+			//Arrange
+			var isDeleted = false;
+
+			//Act
+			var result = _clientSettingsRepo.IsDeleted(0, false);
+
+			//Assert
+			Assert.IsInstanceOfType(result, typeof(IClientSettings));
+			Assert.IsNull(result);
+		}
+		#endregion
+
+		#region Edit Test
+		[TestMethod]
+		public void Edit_NewClientSettingsValidClientId_true()
+		{
+			//Arrange
+			var clientSettings = _genericSettings.One();
+
+			//Act
+			var result = _clientSettingsRepo.Edit(clientSettings, 1);
+
+			//Assert
+			Assert.IsInstanceOfType(result, typeof(IClientSettings));
+			Assert.IsNull(result);
+			Assert.AreSame(result, clientSettings);
+		}
+
+		[TestMethod]
+		public void Edit_NewClientSettingsInvalidClientId_False()
+		{
+			//Arrange
+			var clientSettings = _genericSettings.One();
+
+			//Act
+			var result = _clientSettingsRepo.Edit(clientSettings, 0);
+
+			//Assert
+			Assert.IsInstanceOfType(result, typeof(IClientSettings));
+			Assert.IsNull(result);
+			Assert.AreSame(result, clientSettings);
+		}
+
+		#endregion
+
+		#region SetOpenClosedTimes Test
+		[TestMethod]
+		public void SetOpenClosedTimes_PassValidClientIdAndDateTime_True()
+		{
+			//Arrange
+			var openingTime = new DateTime(2018, 1, 1, 8, 0, 0);
+			var closingTime = new DateTime(2018, 1, 1, 17, 0, 0);
+
+			//Act
+			var result = _clientSettingsRepo.SetOpenClosedTimes(openingTime, closingTime, 1);
+
+			//Assert
+			Assert.IsInstanceOfType(result, typeof(IClientSettings));
+			Assert.IsNotNull(result);
+			Assert.AreSame(result.OpenTime, openingTime);
+			Assert.AreSame(result.CloseTime, closingTime);
+		}
+
+		[TestMethod]
+		public void SetOpenClosedTimes_PassInvalidClientIdAndDateTime_False()
+		{
+			//Arrange
+			var openingTime = new DateTime(2018, 1, 1, 8, 0, 0);
+			var closingTime = new DateTime(2018, 1, 1, 17, 0, 0);
+
+			//Act
+			var result = _clientSettingsRepo.SetOpenClosedTimes(openingTime, closingTime, 0);
+
+			//Assert
+			Assert.IsInstanceOfType(result, typeof(IClientSettings));
+			Assert.IsNull(result);
+		}
+		#endregion
+
+		#region AddTotalUser Test
+
+		[TestMethod]
+		public void AddTotalUsers_AddNumberOfNewUsers_True()
+		{
+			//Arrange
+			var clientSettings = _genericSettings.One();
+
+			//Act
+
+
+			//Assert
+
+		}
+
+		#endregion
+
+		private int AddClient()
+		{
+			var client = _clients.One(0);
+			Reset();
+			_db.Clients.Add(client);
+			((StockTrackerContext)_db).SaveChanges();
+
+			return client.ClientId;
+		}
+
+		private void Reset()
+		{
+			((StockTrackerContext)_db).Database.EnsureDeleted();
+		}
+	}
 }
--- StockTracker/Clients/ClientSettingsRepo.cs
+++ StockTracker/Clients/ClientSettingsRepo.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StockTracker.Context;
 using StockTracker.Context.Interface;
 using StockTracker.Interface.Models.Clients;
+using StockTracker.Model.Clients;
 using StockTracker.Repository.Interface.Clients;
 
 namespace StockTracker.Repository.Clients
 {
-    public class ClientSettingsRepo 
+    public class ClientSettingsRepo : IClientSettingsRepo
     {
 	    private IStockTrackerContext _db;
 
@@ -18,6 +20,58 @@ namespace StockTracker.Repository.Clients
 		    _db = db;
 	    }
 
-	 
+
+	    public IClientSettings AddClientSettings(IClientSettings settings)
+	    {
+		    try
+		    {
+			    var currentClient = GetClient(settings.ClientId);
+
+			    if (currentClient == null)
+				    return null;
+
+			    _db.ClientSettings.Add((ClientSettings)settings);
+				var result = ((StockTrackerContext) _db).SaveChanges();
+			    if (result == 0)
+				    return null;
+
+			    return _db.ClientSettings.FirstOrDefault(i => i.ClientId == settings.ClientId);
+		    }
+		    catch (Exception e)
+		    {
+				//TODO: ADD LOGGING
+			    return null;
+		    }
+	    }
+
+	    public IClientSettings IsActive(int clientId, bool isActive)
+	    {
+		    throw new NotImplementedException();
+	    }
+
+	    public IClientSettings IsDeleted(int clientId, bool isDeleted)
+	    {
+		    throw new NotImplementedException();
+	    }
+
+	    public IClientSettings Edit(IClientSettings settings, int clientId)
+	    {
+		    throw new NotImplementedException();
+	    }
+
+	    public IClientSettings SetOpenClosedTimes(DateTime openTime, DateTime closedTime, int coreClientId)
+	    {
+		    throw new NotImplementedException();
+	    }
+
+	    public IClientSettings AddTotalUsers(int addUsers)
+	    {
+		    throw new NotImplementedException();
+	    }
+
+	    private IClient GetClient(int clientId)
+	    {
+		    return _db.Clients.FirstOrDefault( i=> i.ClientId == clientId);
+	    }
     }
 }
