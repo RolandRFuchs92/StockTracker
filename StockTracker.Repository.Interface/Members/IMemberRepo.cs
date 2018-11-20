ab3dfd52de56a1ff05e6126f31947a265fbe6a92
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-11-20 20:59:53 +0200

    member repo test

---
+++ StockTracker.Repository.Interface/Members/IMemberRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockTracker.Interface.Models.Member;
+using StockTracker.Model.Member;
+
+namespace StockTracker.Repository.Interface.Members
+{
+    public interface IMemberRepo
+    {
+	    IMember Add(IMember member);
+	    IMember Edit(IMember member);
+	    IMember Toggle(int memberId, bool isActive);
+	    IMember LastActiveDate(int memberId);
+    }
+}
---
+++ StockTracker/Member/MemberRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockTracker.Repository.Interface.Members;
+
+namespace StockTracker.Repository.Member
+{
+    public class MemberRepo 
+    {
+        
+    }
+}
