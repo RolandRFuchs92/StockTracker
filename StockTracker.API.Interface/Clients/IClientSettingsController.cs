9b0bb7a18474cade09a4c52365bbb804c78ddadd
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-11-22 07:24:41 +0200

    Created contracts for the interface.

--- StockTracker.API.Interface/Clients/IClientSettingsController.cs
+++ StockTracker.API.Interface/Clients/IClientSettingsController.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StockTracker.Interface.Models.Clients;
 
 namespace StockTracker.API.Interface.Clients
 {
     public interface IClientSettingsController
     {
-        
+        IActionResult Add(IClientSettings settings);
+        IActionResult IsActive(int clientId, bool isActive);
+        IActionResult IsDeleted(int clientId, bool isDeleted);
+        IActionResult Edit(IClientSettings settings);
+        IActionResult SetBusinessHours(DateTime? openTime, DateTime? closeTime, int clientId);
+        IActionResult AddTotalUsers(int clientId, int userCount);
     }
 }
