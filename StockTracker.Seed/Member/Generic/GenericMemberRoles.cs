6783e284c507f29a3fe5c9dfb442b5fa50341f87
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-11-24 20:15:40 +0200

    Added GenericMemberRole

---
+++ StockTracker.Seed/Member/Generic/GenericMemberRoles.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockTracker.Model.Members;
+using StockTracker.Seed.Interface;
+
+namespace StockTracker.Seed.Member.Generic
+{
+    public class GenericMemberRoles : IGeneric<MemberRole>
+    {
+        public MemberRole[] All()
+        {
+            return new[]
+            {
+                new MemberRole
+                {
+                    IsActive = true,
+                    MemberRoleId = 1,
+                    MemberRoleName = "Manager"
+                },
+                new MemberRole
+                {
+                    IsActive = true,
+                    MemberRoleId = 2,
+                    MemberRoleName = "Head Cheff"
+                },
+                new MemberRole
+                {
+                    IsActive = true,
+                    MemberRoleId = 3,
+                    MemberRoleName = "Cheff"
+                },
+                new MemberRole
+                {
+                    IsActive = true,
+                    MemberRoleId = 4,
+                    MemberRoleName = "Waiter"
+                },
+            };
+        }
+
+        public MemberRole One()
+        {
+            return All()[0];
+        }
+
+        public MemberRole One(int index)
+        {
+            return All()[index];
+        }
+    }
+}
