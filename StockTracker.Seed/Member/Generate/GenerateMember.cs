2241b3fe2cae3c86dacdc9ecd49e84beb56931f2
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-11-24 20:36:48 +0200

    Added a series of database entries to create a full member for testing perposes.

---
+++ StockTracker.Seed/Member/Generate/GenerateMember.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockTracker.Context;
+using StockTracker.Context.Interface;
+using StockTracker.Model.Clients;
+using StockTracker.Model.Members;
+using StockTracker.Model.Persons;
+using StockTracker.Seed.Clients.Generic;
+using StockTracker.Seed.Interface;
+using StockTracker.Seed.Member.Generic;
+using StockTracker.Seed.Persons;
+
+namespace StockTracker.Seed.Member.Generate
+{
+    public class GenerateMember : IGenerate
+    {
+        private IStockTrackerContext _db;
+        private Model.Members.Member[] _memberList;
+        private Client[] _clientList;
+        private MemberRole[] _memberRoleList;
+        private Person[] _personList;
+
+        public GenerateMember(IStockTrackerContext db)
+        {
+            _db = db;
+            _memberList = new GenericMember().All();
+            _clientList = new GenericClients().All();
+            _memberRoleList = new GenericMemberRoles().All();
+            _personList = new GenericPerson().All();
+        }
+
+        public void Generate()
+        {
+            Truncate();
+
+            _db.Members.AddRange(_memberList);
+            _db.Clients.AddRange(_clientList);
+            _db.MemberRoles.AddRange(_memberRoleList);
+            _db.Persons.AddRange(_personList);
+
+            ((StockTrackerContext) _db).SaveChanges();
+        }
+
+        private void Truncate()
+        {
+            ((StockTrackerContext)_db).Database.EnsureDeleted();
+        }
+    }
+}
