2906d71158613214c07791e2dc19d0ce35f3a9e7
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-11-24 21:00:02 +0200

    Completed Edit Test

--- StockTracker.Tests/StockTracker/Member/MemberRepoTest.cs
+++ StockTracker.Tests/StockTracker/Member/MemberRepoTest.cs
@@ -10,6 +10,8 @@ using StockTracker.Interface.Models.Member;
 using StockTracker.Repository.Interface.Members;
 using StockTracker.Context;
 using StockTracker.Context.Interface;
+using StockTracker.Seed.Member.Generate;
+using StockTracker.Seed.Member.Generic;
 
 namespace StockTracker.Repository.Test.StockTracker.Member
 {
@@ -17,25 +19,53 @@ namespace StockTracker.Repository.Test.StockTracker.Member
     {
         private IMemberRepo _memberRepo;
         private IStockTrackerContext _db;
+        private GenerateMember _generateMembers;
+        private GenericMember _genericMember;
 
         public MemberRepoTest()
         {
             _db = new TestDb().Db;
+            _generateMembers = new GenerateMember(_db);
+            _genericMember = new GenericMember();
         }
 
+        #region Add Test
         [TestMethod]
         public void Add_PassValidClientAndMember_NewMemberObject()
         {
             //Arrange
-            var member = new Model.Members.Member();
-            
-
+            _generateMembers.Generate();
+            var member = _genericMember.One();
+            ((StockTrackerContext)_db).Members.RemoveRange(_genericMember.All());
+                    
             //Act
-            //var result = _memberRepo.Add();
+            var result = _memberRepo.Add(member);
 
             //Assert
-
+            Assertions(result);
         }
+
+        [TestMethod]
+        public void Add_PassInvalidClientWithMember_Null()
+        {
+            //Arrange
+            _generateMembers.Generate();
+            var member = new Model.Members.Member();
+            ((StockTrackerContext)_db).Members.RemoveRange(_genericMember.All());
+
+            //Act
+            var result = _memberRepo.Add(member);
+
+            //Assert
+            Assertions(result, false);
+        }
+        #endregion
+
+        #region Edit Test
+
+        
+
+        #endregion
 
         private IStockTrackerContext StockTracker (Expression<Func<IStockTrackerContext, IMember>> method, IMember result)
         {
@@ -43,6 +73,18 @@ namespace StockTracker.Repository.Test.StockTracker.Member
             moq.Setup(method).Returns(result);
             
             return moq.Object;
+        }
+
+        private void Assertions(IMember result, bool isValid = true)
+        {
+            if (isValid)
+            {
+                Assert.IsInstanceOfType(result, typeof(IMember));
+            }
+            else
+            {
+                Assert.IsNull(result);
+            }
         }
 
     }
