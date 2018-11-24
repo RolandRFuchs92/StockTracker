844e38065dd9aae5a39b8d22f7490fee7d5f0056
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-11-24 20:25:37 +0200

    Added GenericPerson

---
+++ StockTracker.Seed/Persons/GenericPerson.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockTracker.Model.Persons;
+using StockTracker.Seed.Interface;
+
+namespace StockTracker.Seed.Persons
+{
+    public class GenericPerson : IGeneric<Person>
+    {
+        public Person[] All()
+        {
+            return new[]
+            {
+                new Person
+                {
+                    Email = "test@test.co.za",
+                    Mobile = "0846004321",
+                    PersonId = 1,
+                    PersonName = "TestName1",
+                    PersonSurname = "TestSurname1",
+                    WhatsApp = "0730521234"
+                },
+                new Person
+                {
+                    PersonId = 2,
+                    Email = "secondmail@mail.co.za",
+                    Mobile = "0843006543",
+                    WhatsApp = "",
+                    PersonName = "TestName2",
+                    PersonSurname = "TestSurname2"
+                },
+                new Person
+                {
+                    PersonId = 3,
+                    Email = "thenewthirdnumber@gmail.com",
+                    Mobile = "0123456789",
+                    WhatsApp = "",
+                    PersonName = "Samuel",
+                    PersonSurname = "Jackson"
+                }
+            };
+        }
+
+        public Person One()
+        {
+            return All()[0];
+        }
+
+        public Person One(int index)
+        {
+            return All()[index];
+        }
+    }
+}
