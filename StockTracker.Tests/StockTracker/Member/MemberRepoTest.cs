f9f923bc4899d827e0f54938afd52a82d19db3d3
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-11-25 19:59:55 +0200

    Added MemberRepo Tests for Edit, ChangeRole, ChangeClient

--- StockTracker.Tests/StockTracker/Member/MemberRepoTest.cs
+++ StockTracker.Tests/StockTracker/Member/MemberRepoTest.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Remotion.Linq.Utilities;
 using StockTracker.Interface.Models.Member;
 using StockTracker.Repository.Interface.Members;
 using StockTracker.Context;
@@ -50,7 +51,7 @@ namespace StockTracker.Repository.Test.StockTracker.Member
         {
             //Arrange
             _generateMembers.Generate();
-            var member = new Model.Members.Member();
+            var member = _genericMember.One();
             ((StockTrackerContext)_db).Members.RemoveRange(_genericMember.All());
 
             //Act
@@ -62,11 +63,144 @@ namespace StockTracker.Repository.Test.StockTracker.Member
         #endregion
 
         #region Edit Test
+        [TestMethod]
+        public void Edit_PassValidClient_FullMember()
+        {
+            //Arrange
+            _generateMembers.Generate();
+            var member = _genericMember.One();
+            var isMemberActive = false;
+            var clientId = 3;
+            member.IsActive = isMemberActive;
+            member.ClientId = clientId;
 
-        
+            //Act
+            var result = _memberRepo.Edit(member);
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(IMember));
+            Assert.AreEqual(result.IsActive, isMemberActive);
+            Assert.AreEqual(result.ClientId, clientId);
+        }
+
+        [TestMethod]
+        public void Edit_PassValidClient_NullMember()
+        {
+            //Arrange
+            _generateMembers.Generate();
+            var member = _genericMember.One();
+            member.ClientId = 0;
+
+            //Act
+            var result = _memberRepo.Edit(member);
+
+            //Assert
+            Assert.IsNull(result);
+        }
 
         #endregion
 
+        #region ChangeRole Test
+        [TestMethod]
+        public void ChangeRole_PassValidRole_PassMemberWithNewMemberRole()
+        {
+            //Arrange
+            _generateMembers.Generate();
+            var newRoleId = 3;
+            var memberId = 1;
+
+            //Act
+            var result = _memberRepo.ChangeRole(memberId, newRoleId);
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(IMember));
+            Assert.AreEqual(result.MemberRoleId, newRoleId);
+        }
+
+        [TestMethod]
+        public void ChangeRole_PassInvalidMemberId_ReturnNull()
+        {
+            //Arrange
+            _generateMembers.Generate();
+            var memberId = 0;
+
+            //Act
+            var result = _memberRepo.ChangeRole(memberId, 3);
+
+            //Assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void ChangeRole_PassInvalidMemberRoleId_ReturnNull()
+        {
+            //Arrange
+            _generateMembers.Generate();
+            var memberId = 1;
+            var memberRoleId = 100;
+
+            //Act
+            var result = _memberRepo.ChangeRole(memberId, memberRoleId);
+
+            //Assert
+            Assert.IsNull(result);
+        }
+        #endregion
+
+        #region ChangeClient Test
+        [TestMethod]
+        public void ChangeClient_PassValidMemberAndClient_ChangedMemberWithClientReference()
+        {
+            //Arrange
+            _generateMembers.Generate();
+            var memberId = 1;
+            var clientId = 2;
+
+            //Act
+            var result = _memberRepo.ChangeClient(memberId, clientId);
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(IMember));
+            Assert.AreEqual(result.ClientId, clientId);
+        }
+
+        [TestMethod]
+        public void ChangeClient_PassInvalidMember_ReturnNull()
+        {
+            //Arrange
+            _generateMembers.Generate();
+            var memberId = 0;
+            var clientId = 1;
+
+            //Act
+            var result = _memberRepo.ChangeClient(memberId, clientId);
+
+            //Assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void ChangeClient_PassInvalidClientId_ReturnNull()
+        {
+            //Arrange
+            _generateMembers.Generate();
+            var memberId = 1;
+            var clientId = 0;
+
+            //Act
+            var result = _memberRepo.ChangeClient(memberId, clientId);
+
+            //Assert
+            Assert.IsNull(result);
+        }
+        #endregion
+
+        #region LastActiveDate Test
+
+
+        #endregion
+
+        #region Dry Code
         private IStockTrackerContext StockTracker (Expression<Func<IStockTrackerContext, IMember>> method, IMember result)
         {
             var moq = new Mock<IStockTrackerContext>();
@@ -86,6 +220,6 @@ namespace StockTracker.Repository.Test.StockTracker.Member
                 Assert.IsNull(result);
             }
         }
-
+        #endregion
     }
 }
