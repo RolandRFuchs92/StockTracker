5dae4ac98ce9365bd4c975f4f5b4e41da8183ada
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-12-10 19:36:52 +0200

    readded using statements and references

--- StockTracker.BusinessLogic.Test/Clients/ClientLogicTest.cs
+++ StockTracker.BusinessLogic.Test/Clients/ClientLogicTest.cs
@@ -16,8 +16,6 @@ using StockTracker.Context;
 using StockTracker.Interface.Models.Clients;
 using StockTracker.Repository.Clients;
 using StockTracker.Repository.Interface.Clients;
-using StockTracker.Repository.Test;
-using StockTracker.Seed.Clients;
 using StockTracker.Seed.Clients.Generic;
 
 namespace StockTracker.BusinessLogic.Test.Clients
