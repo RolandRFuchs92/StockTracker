512eb2284d1a59eae62349da99563096cce1a8ee
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-12-10 21:42:06 +0200

    Added the inital bits for the stockrepo test

--- StockTracker.Repository.Tests/StockTracker/Stock/StockCoreRepoTest.cs
+++ StockTracker.Repository.Tests/StockTracker/Stock/StockCoreRepoTest.cs
@@ -4,11 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using StockTracker.Adapter.Interface.Logger;
+using StockTracker.BusinessLogic.Test.Utils;
+using StockTracker.Context;
+using StockTracker.Model.Stock;
+using StockTracker.Repository.Interface.Stock;
+using StockTracker.Repository.Stock;
 
 namespace StockTracker.Repository.Test.StockTracker.Stock
 {
     [TestClass]
     public class StockCoreRepoTest
     {
+        private IStockCoreRepo _repo;
+        private StockTrackerContext _db;
+        private GenericLoggerCheck<StockCoreRepo> _check;
+        private Mock<ILoggerAdapter<StockCoreRepo>> _log;
+
+        public StockCoreRepoTest()
+        {
+            _db = new TestDbFactory().Db();
+            _check = new GenericLoggerCheck<StockCoreRepo>();
+            _log = _check.Mock;
+        }
+
+        [TestMethod]
+        public void Add_ValidStockCoreObject_ReturnNewStockCoreItem()
+        {
+            //Arrange
+ 
+            //Act
+
+            //Assert
+
+        }
+
+        private IStockCoreRepo GetRepo()
+        {
+            var moq = new Mock<IStockCoreRepo>();
+
+            return moq.Object;
+        }
     }
 }
