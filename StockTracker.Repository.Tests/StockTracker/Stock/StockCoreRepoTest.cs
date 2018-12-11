c6914064a7d9ba4049df4da976ff92f397c21ddc
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-12-11 20:58:46 +0200

    Leadned how to set a property without knowing what the property is thats going to change! super cool dude!

--- StockTracker.Repository.Tests/StockTracker/Stock/StockCoreRepoTest.cs
+++ StockTracker.Repository.Tests/StockTracker/Stock/StockCoreRepoTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -11,6 +12,7 @@ using StockTracker.Context;
 using StockTracker.Model.Stock;
 using StockTracker.Repository.Interface.Stock;
 using StockTracker.Repository.Stock;
+using StockTracker.Seed.Stock;
 
 namespace StockTracker.Repository.Test.StockTracker.Stock
 {
@@ -21,24 +23,69 @@ namespace StockTracker.Repository.Test.StockTracker.Stock
         private StockTrackerContext _db;
         private GenericLoggerCheck<StockCoreRepo> _check;
         private Mock<ILoggerAdapter<StockCoreRepo>> _log;
+        private GenericStockCore _genericStock;
 
         public StockCoreRepoTest()
         {
             _db = new TestDbFactory().Db();
             _check = new GenericLoggerCheck<StockCoreRepo>();
+            _genericStock = new GenericStockCore();
             _log = _check.Mock;
         }
 
+        #region Add Test
         [TestMethod]
         public void Add_ValidStockCoreObject_ReturnNewStockCoreItem()
         {
             //Arrange
- 
+            var repo = GetRepo();
+            var stockItem = _genericStock.One();
+
             //Act
+            var result = repo.Add(stockItem);
 
             //Assert
-
+            Asserts(result);
+            _check.Success();
         }
+
+        [TestMethod]
+        public void Add_InvalidStockCategory_LogError()
+        {
+            var stockItem = _genericStock.One();
+            Add_Tests(stockItem, "StockCategoryId", 0);
+        }
+
+        private void Add_Tests(StockCore stockItem, string propertyName, int val)
+        {
+            //Arrange
+            var repo = GetRepo();
+            stockItem.GetType().GetProperty(propertyName).SetValue(stockItem, val);
+
+            //Act
+            var result = repo.Add(stockItem);
+
+            //Assert
+            Asserts(result, false);
+            _check.Error();
+        }
+
+
+        #endregion
+
+        #region MyRegion
+        private void Asserts(StockCore result , bool isSuccess = true)
+        {
+            if (isSuccess)
+            {
+                Assert.IsNotNull(result);
+            }
+            else
+            {
+                Assert.IsNull(result);
+            }
+        }
+
 
         private IStockCoreRepo GetRepo()
         {
@@ -46,5 +93,8 @@ namespace StockTracker.Repository.Test.StockTracker.Stock
 
             return moq.Object;
         }
+
+        #endregion
+
     }
 }
