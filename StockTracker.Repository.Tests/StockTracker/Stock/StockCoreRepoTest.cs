be2d7c48d3ce462802ad0bdce96dacb66edd93de
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2018-12-12 07:32:53 +0200

    Added some generic tests

--- StockTracker.Repository.Tests/StockTracker/Stock/StockCoreRepoTest.cs
+++ StockTracker.Repository.Tests/StockTracker/Stock/StockCoreRepoTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -85,7 +86,6 @@ namespace StockTracker.Repository.Test.StockTracker.Stock
         #endregion
 
         #region Edit Test
-
         [TestMethod]
         public void Edit_PassValidStockObject_ReturnModifiedStockItem()
         {
@@ -97,7 +97,6 @@ namespace StockTracker.Repository.Test.StockTracker.Stock
 
             Edit_Tests_Success(testValues);
         }
-
 
         [TestMethod]
         public void Edit_PassOnlyStockCoreIdAndNewCategoryId_ReturnFullStockItemAndLogSuccess()
@@ -199,7 +198,7 @@ namespace StockTracker.Repository.Test.StockTracker.Stock
         private void Edit_Tests_Error(Dictionary<string, string> propertyValues, bool isExceptionCheck = false)
         {
             //Arrange
-            var repo = GetRepo(i => i.StockCores.Select(It.IsAny<Func<StockCore, StockCore>>()));
+            var repo = GetRepo(true);
             var stockItem = _genericStock.One();
 
             foreach (var item in propertyValues)
@@ -226,8 +225,62 @@ namespace StockTracker.Repository.Test.StockTracker.Stock
 
         #endregion
 
-        #region MyRegion
-        private void Asserts(StockCore result , bool isSuccess = true)
+        #region ChangeCategory Test
+
+        [TestMethod]
+        public void ChangeCategory_PassValidStockIdAndDifferentCategory_NewStockItemAndLogSuccess()
+        {
+            ChangeCategory_Test(new Dictionary<string, string>{ { "StockCategoryId", "3"}});
+        }
+
+        [TestMethod]
+        public void ChangeCategory_PassInvalidCategoryId_ReturnNullLogGenericError()
+        {
+            ChangeCategory_Test(new Dictionary<string, string>{{ "StockCategoryId", "1" }}, false);
+        }
+
+        [TestMethod]
+        public void ChangeCategory_ThrowsException_ReturnNullLogGenericErrorException()
+        {
+            ChangeCategory_Test(new Dictionary<string, string> { { "StockCategoryId", "1" } }, false, true);
+        }
+
+        private void ChangeCategory_Test(Dictionary<string,string> newVals, bool isSuccess = true, bool exceptionTest = false)
+        {
+            //Arrange
+            var repo = GetRepo(exceptionTest);
+            var stockCore = ModifyStockCore(newVals);
+
+            //Act
+            var result = repo.ChangeCategory(stockCore.StockCoreId, int.Parse(newVals["StockCategoryId"]));
+
+            //Assert
+            Asserts(result, isSuccess);
+            
+            _check.Success();
+        }
+
+        #endregion
+
+        #region ChangeStockType Test
+
+        [TestMethod]
+        public void ChangeStockType_PassValidStockCodeIdAndValidStockType_LogSuccessReturnNewStockCode()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+        }
+
+        #endregion
+
+        #region Dry Code
+        private void Asserts(StockCore result, bool isSuccess = true)
         {
             if (isSuccess)
             {
@@ -239,13 +292,46 @@ namespace StockTracker.Repository.Test.StockTracker.Stock
             }
         }
 
+        private void Asserts(StockCore result, Dictionary<string,string> compare, bool isSuccess = true)
+        {
+            if (isSuccess)
+            {
+                Assert.IsNotNull(result);
+                foreach (var item in compare)
+                {
 
-        private IStockCoreRepo GetRepo(Expression<Action<IStockTrackerContext>> customContextBehavior) 
+                }
+            }
+            else
+            {
+                Assert.IsNull(result);
+            }
+        }
+
+        private StockCore ModifyStockCore(Dictionary<string, string> newVals)
+        {
+            var stockCore = _genericStock.One();
+
+            foreach (var item in newVals)
+            {
+                var isInt = int.TryParse(item.Value, out int itemInt);
+
+                if(isInt)
+                    stockCore.GetType().GetProperty(item.Key).SetValue(stockCore.GetType(), itemInt);
+                else
+                    stockCore.GetType().GetProperty(item.Key).SetValue(stockCore.GetType(), item.Value);
+            }
+
+            return stockCore;
+        }
+
+        private IStockCoreRepo GetRepo(bool mustThrowError = false) 
         {
             var repo = new Mock<IStockCoreRepo>().Object;
 
             var db = new Mock<IStockTrackerContext>();
-            db.Setup(customContextBehavior).Throws(new Exception());
+            if(mustThrowError)
+            db.Setup(i => i.StockCores).Throws(new Exception());
 
             return repo;
         }
