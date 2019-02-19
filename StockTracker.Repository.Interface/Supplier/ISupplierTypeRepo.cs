e1e0fe0971d7cbf861345286837b2d66ccbb3716
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2019-02-19 20:19:40 +0200

    Added the suppliertestrepo

---
+++ StockTracker.Repository.Interface/Supplier/ISupplierTypeRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockTracker.Interface.Models.Supplier;
+
+namespace StockTracker.Repository.Interface.Supplier
+{
+    public interface ISupplierTypeRepo
+    {
+	    ISupplierType Add(ISupplierType supplier);
+	    ISupplierType Edit(ISupplierType supplier);
+	    bool Delete(int supplierTypeId);
+	    List<ISupplierType> List();
+    } 
+}
