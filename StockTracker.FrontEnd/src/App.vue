45a630caf434696f0f5e4d3105f776fe43e86cc4
Roland Fuchs <roland@ix.co.za>
2019-02-22 16:57:36 +0200

    Added some stuff to the new front end thingy

--- StockTracker.FrontEnd/src/App.vue
+++ StockTracker.FrontEnd/src/App.vue
@@ -1,21 +1,22 @@
 <template>
-    <div id="app">
-        <Home msg="Hello world!" />
-								<Hit-Server name="boi" />
-    </div>
+		<div id="app">
+				<Home msg="Hello world!" />
+				<Hit-Server name="boi" />
+		</div>
 </template>
 
 <script>
-    import Home from './components/Home.vue';
-				import HitServer from './components/HitServer.vue';
+		import Home from './components/Home.vue';
+		import HitServer from './components/HitServer.vue';
+		//import axios from "axios";
 
-    export default {
-        name: 'app',
-        components: {
-										Home,
-										HitServer
-								}
-    };
+		export default {
+				name: 'app',
+				components: {
+						Home,
+						HitServer
+				}
+		};
 </script>
 
 <style>
---
+++ StockTracker.FrontEnd/src/Javascript/axios.js
@@ -0,0 +1,17 @@
+﻿// in store.js actions
+axiosSomething(context) {
+		return new Promise((resolve, reject) => {
+				axios
+						.get(
+								`http://axiossomething.please:3001/userinfo`
+						)
+						.then(
+								({ data, status, statusText }) => {
+										context.commit("mutateUserInfo", data);
+										console.log("Trades:", status, statusText);
+										resolve(data);
+								},
+								error => reject(error)
+						);
+		});
+}
