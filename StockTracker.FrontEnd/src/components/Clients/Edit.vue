53bf62229ab8366df62cbc2f1875a7ad39d02121
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2019-03-03 07:19:08 +0200

    Setup client edit page.

--- StockTracker.FrontEnd/src/components/Clients/Edit.vue
+++ StockTracker.FrontEnd/src/components/Clients/Edit.vue
@@ -1,11 +1,53 @@
 <template>
-  <div id="client-edit">
+  <b-col sm="6">
     <h4>Client Edit</h4>
-    
-  </div>
+    <br>
+    <b-form @submit="onSubmit" @reset="onReset">
+      <b-form-group label="Client Name" label-for="client-name">
+        <b-form-input id="client-name" type="text" v-model="form.name" required placeholder="Client Name"></b-form-input>
+      </b-form-group>
+
+      <b-form-group label="Email" label-for="client-email">
+        <b-form-input id="client-email" type="email" v-model="form.email" required placeholder="Client Email"></b-form-input>
+      </b-form-group>
+
+      <b-form-group label="Mobile" label-for="client-contact-number">
+        <b-form-input id="client-contact-number" type="text" v-model="form.contactnumber" required placeholder="Client Contact Number"></b-form-input>
+      </b-form-group>
+
+      <b-form-group label="Address" label-for="client-address"> 
+        <b-form-input id="client-address" type="text" v-model="form.address" required placeholder="Address"></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="default">reset</b-button>
+    </b-form>
+  </b-col>
 </template>
 <script>
 export default {
-  name: 'client-edit'
+  name: 'client-edit',
+  data() {
+    return {
+      form: {
+        name: '',
+        email: '',
+        contactnumber: '',
+        address: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit(event){
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(event){
+      this.form.email = '';
+      this.form.name = '';
+      this.form.contactnumber = '';
+      this.form.address = '';
+    }
+  }
 }
 </script>
