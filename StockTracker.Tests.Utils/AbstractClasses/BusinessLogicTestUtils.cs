89b7c59879f89afa75c65c8d2fec0399e3d99067
RolandRFuchs92 <rolandrfuchs92@gmail.com>
2019-03-03 21:59:36 +0200

    Added some simplified logic and added ClientLogictest

--- StockTracker.Tests.Utils/AbstractClasses/BusinessLogicTestUtils.cs
+++ StockTracker.Tests.Utils/AbstractClasses/BusinessLogicTestUtils.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
 using StockTracker.Context;
 using StockTracker.Tests.Utils.Context;
 using StockTracker.Tests.Utils.MockVerifiers;
@@ -11,22 +12,27 @@ namespace StockTracker.Tests.Utils.AbstractClasses
 {
 	public abstract class BusinessLogicTestUtils<TService, TRepo> : CollatedAsserts<TService>
 		where TService : class
+		where TRepo : class
+
 	{
 		protected StockTrackerContext _db;
 		protected TRepo _repo;
-		protected GenericLoggerCheck<TRepo> _repoLog;
 		protected TService _service;
 
 		protected BusinessLogicTestUtils()
 		{
 			_db = new TestDbFactory().Db();
-			_repoLog = new GenericLoggerCheck<TRepo>();
-			_repo = (TRepo)Activator.CreateInstance(typeof(TRepo), _repo, _repoLog.Mock.Object);
+			_repo = CreateRepo();
 		}
 
-		public virtual void CreateInstance()
+		public virtual TRepo CreateRepo()
 		{
-			_service = (TService)Activator.CreateInstance(typeof(TService), _repo, _log);
+			return new Mock<TRepo>().Object;
+		}
+
+		public virtual void CreateInstance(TRepo repo = null)
+		{
+			_service = (TService)Activator.CreateInstance(typeof(TService), _repo, _log.Mock.Object);
 		}
 
 		public virtual void CreateResult(string methodName, params object[] parameters)
